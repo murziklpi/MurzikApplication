935329c9e3e57f1de16a4ab94cf5b378dc4b52b1
Victor.Sobennikov <murzik_lpi@mail.ru>
2020-01-24 17:29:34 +0300

    24-01

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,34 @@
 package com.example.myapplication
 
-import android.content.Context
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-//import android.R
+import android.widget.Button
 import android.widget.TextView
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
 
-    val tVBoy: TextView=findViewById(R.id.textViewBoy);
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnBoy = findViewById<Button>(R.id.buttonBoy)
+        btnBoy.setOnClickListener(clickListenerBoy)
+        val btnGirl = findViewById<Button>(R.id.buttonGirl)
+        btnBoy.setOnClickListener(clickListenerGirl)
     }
 
-    fun onButtonGirlClick(view: View) {
-        tVBoy.setTextColor(ContextCompat.getColor(tVBoy?.context,R.color.colorPress))
+    private val clickListenerBoy: View.OnClickListener = View.OnClickListener { _ ->
+        setContentView(R.layout.activity_main)
+        val tVBoy = findViewById(R.id.textViewBoy) as TextView
+        tVBoy.setTextColor(R.color.colorPress.dec())
     }
+
+    private val clickListenerGirl: View.OnClickListener = View.OnClickListener { _ ->
+        setContentView(R.layout.activity_main)
+        val tVBoy = findViewById(R.id.textViewGirl) as TextView
+        tVBoy.setTextColor(R.color.colorPress.dec())
+    }
+
 }
