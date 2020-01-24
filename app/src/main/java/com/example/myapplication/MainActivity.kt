573a66197450377f271922c0248d9647cf133ce5
murziklpi <murzik_lpi@mail.ru>
2020-01-24 07:30:23 +0300

    change 20200123

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,17 +9,18 @@ import android.widget.TextView
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
 
+    val tVBoy: TextView=findViewById(R.id.textViewBoy);
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun onButtonGirlClick(view: View) {
-        //val infoTextView = findViewById("") as TextView
+        tVBoy.setTextColor(ContextCompat.getColor(tVBoy?.context,R.color.colorPress))
     }
 }
